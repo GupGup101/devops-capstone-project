8d0c1cb6fae30d4a323cf1bac4dd24946763182b
GupGup <infocraft39@gmail.com>
2025-03-21 20:27:18 -0400

    adding read route

--- service/routes.py
+++ service/routes.py
@@ -64,11 +64,26 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 
 
-######################################################################
+#####################################################################
 # READ AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Read  an Account
+    This endpoint will read an Account based on the ID
+    """
+    app.logger.info("Request to create an Account")
 
-# ... place you code here to READ an account ...
+    # use the Account.find() method to find the account
+    account = Account.find(account_id)
+    
+    # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    
+    # return the serialize() version of the account with a return code of status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
